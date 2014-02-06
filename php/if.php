9471b2970f4d712f5db158eb0bf24a6a27a35671
Michael Girdley <Michael@codeup.com>
2014-02-06 14:35:29 -0600

    New examples added.

---
+++ php/if.php
@@ -0,0 +1,43 @@
+<?php
+
+$a = 5;
+$b = 10;
+$c = '10';
+
+// Shorten the next 2 statements into an if/else
+if ($a < $b) {
+    // output the appropriate result
+    echo "$a is less than $b\n";
+} elseif ($a > $b) {
+    // output the appropriate result
+    echo "$a is greater than $b\n";
+}
+
+// Shorten the next 2 statements into an if/else
+if ($b >= $c) {
+    // output the appropriate result
+    echo "$b is greater than or equal to $c\n";
+} elseif ($b <= $c) {
+    // output the appropriate result
+    echo "$b is is less than or equal to $c\n";
+}
+
+// combine the next 4 conditionals into 
+// one if/else/elseif block that checks in order for:
+// identical, equal, not identical, not equal
+
+if ($b === $c) {
+    // output the appropriate result
+    echo "$b is identical to $c\n";
+} elseif ($b == $c) {
+    // output the appropriate result
+    echo "$b is equal to $c\n";
+} elseif ($b !== $c) {
+    // output the appropriate result
+    echo "$b is not identical to $c\n";
+} elseif ($b != $c) {
+    // output the appropriate result
+    echo "$b is not equal to $c\n";
+}
+
+?>
---
+++ php/simple-if.php
@@ -0,0 +1,12 @@
+<?php
+
+$a = 5;
+$b = 10;
+
+if ($a == $b) {
+    echo "$a is equal to $b";
+} else {
+    echo "$a is not equal to $b";
+}
+
+?>
