0bb1f2188b897a61b6789f93b008ffd50c2001df
Michael Girdley <Michael@codeup.com>
2014-02-08 13:25:33 -0600

    version that only prints scalars.

--- php/foreach.php
+++ php/foreach.php
@@ -4,17 +4,8 @@ $things = array('Sgt. Pepper', "11", null, array(1,2,3), 3.14, "12 + 7", false, 
 
 foreach ($things as $value) {
 
-    echo "Value is a";
+    if (is_scalar($value)) { echo $value . ".\n"; } 
 
-    if (is_integer($value)) { echo "n integer."; } 
-        elseif (is_float($value)) { echo " float."; }
-        elseif (is_bool($value)) { echo " boolean."; }
-        elseif (is_array($value)) { echo "n array."; }
-        elseif (is_null($value)) {echo " null."; }
-        elseif (is_string($value)) { echo " string."; }
-        else {echo " none of the above!"; }
-
-    echo("\n");   
 }
 
 ?>
