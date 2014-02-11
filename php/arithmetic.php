9af9571bec9f7f143a77b3bb48ea3145eff29493
Michael Girdley <Michael@codeup.com>
2014-02-10 19:53:34 -0600

    initial commit.

---
+++ php/arithmetic.php
@@ -0,0 +1,22 @@
+<?php
+
+function add($a, $b) {
+    echo $a + $b;
+}
+
+function subtract($a, $b) {
+    // Add code here
+    echo $a - $b;
+}
+
+function multiply($a, $b) {
+    // Add code here
+    echo $a * $b;
+}
+
+function divide($a, $b) {
+    // Add code here
+	echo $a / $b;
+}
+
+?>
