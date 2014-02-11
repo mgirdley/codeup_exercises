20ddc7cbd0b2fa7e6266c17b95fbbe3c303c44f2
Michael Girdley <Michael@codeup.com>
2014-02-10 19:58:56 -0600

    2nd exercise.

--- php/arithmetic.php
+++ php/arithmetic.php
@@ -1,22 +1,28 @@
 <?php
 
 function add($a, $b) {
-    echo $a + $b;
+    echo $a + $b . PHP_EOL;
 }
 
 function subtract($a, $b) {
     // Add code here
-    echo $a - $b;
+    echo $a - $b . PHP_EOL;
 }
 
 function multiply($a, $b) {
     // Add code here
-    echo $a * $b;
+    echo $a * $b . PHP_EOL;
 }
 
 function divide($a, $b) {
     // Add code here
-	echo $a / $b;
+	echo $a / $b . PHP_EOL;
 }
+
+divide(5,2);
+multiply(3,12039123);
+subtract(123,32);
+add(12,12);
+
 
 ?>
